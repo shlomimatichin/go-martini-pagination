4018852ee82554d2dd2368d9e95c4516b6aa487f
Shlomi Matichin <shlomomatichin@gmail.com>
2017-04-23 21:03:05 +0300

    Bookmarked assisted infinite scroll

--- main.go
+++ main.go
@@ -10,11 +10,14 @@ import (
 )
 
 type Pagination struct {
-	Page    uint
-	Offset  uint
-	PerPage uint
-	total   uint
-	result  []interface{}
+	Page         uint
+	Offset       uint
+	PerPage      uint
+	Bookmark     string
+	nextBookmark string
+	nextOffset   uint
+	total        uint
+	result       []interface{}
 }
 
 var DefaultPerPage uint = 20
@@ -29,6 +32,15 @@ func (pagination *Pagination) SetTotal(total uint) {
 
 func (pagination *Pagination) SetAbort() {
 	pagination.total = math.MaxUint32 - 1
+}
+
+func (pagination *Pagination) IsAborted() bool {
+	return pagination.total == math.MaxUint32-1
+}
+
+func (pagination *Pagination) SetNextBookmark(offset uint, bookmark string) {
+	pagination.nextOffset = offset
+	pagination.nextBookmark = bookmark
 }
 
 func Service(c martini.Context, req *http.Request, r render.Render) {
@@ -51,12 +63,31 @@ func Service(c martini.Context, req *http.Request, r render.Render) {
 		if len(req.URL.Query()["page"]) > 1 {
 			panic("More than one page parameter attached to get url")
 		}
+		if len(req.URL.Query()["offset"]) > 0 {
+			panic("Must not specify both 'page' and 'offset'")
+		}
 		page, err := strconv.ParseUint(req.URL.Query()["page"][0], 10, 32)
 		if err != nil {
 			panic(fmt.Sprintf("Error parsing 'page': %s", err))
 		}
 		pagination.Page = uint(page)
 		pagination.Offset = uint(page * uint64(pagination.PerPage))
+	}
+	if len(req.URL.Query()["offset"]) > 0 {
+		if len(req.URL.Query()["offset"]) > 1 {
+			panic("More than one offset parameter attached to get url")
+		}
+		offset, err := strconv.ParseUint(req.URL.Query()["offset"][0], 10, 32)
+		if err != nil {
+			panic(fmt.Sprintf("Error parsing 'offset': %s", err))
+		}
+		pagination.Offset = uint(offset)
+	}
+	if len(req.URL.Query()["bookmark"]) > 0 {
+		if len(req.URL.Query()["bookmark"]) > 1 {
+			panic("More than one bookmark parameter attached to get url")
+		}
+		pagination.Bookmark = req.URL.Query()["bookmark"][0]
 	}
 	c.Map(&pagination)
 	c.Next()
@@ -71,6 +102,13 @@ func Service(c martini.Context, req *http.Request, r render.Render) {
 		"total":   pagination.total,
 		"page":    pagination.Page,
 		"perpage": pagination.PerPage,
+		"offset":  pagination.Offset,
+	}
+	if len(pagination.nextBookmark) > 0 {
+		resultJSON["next"] = map[string]interface{}{
+			"bookmark": pagination.nextBookmark,
+			"offset":   pagination.nextOffset,
+		}
 	}
 	r.JSON(200, resultJSON)
 }
